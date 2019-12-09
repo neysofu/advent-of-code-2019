36000eb90facd8b64c41401a75f6a644a56f0373
Filippo Costa <filippocosta.italy@gmail.com>
2019-12-09 23:23:03 +0100

    Solve correctly first problem

---
+++ 1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+def calculate_fuel_for_module(n):
+    return n // 3 - 2
+
+#if __name__ == "__main__":
+f = open(sys.argv[1], "r").read()
+total = 0
+for line in f.splitlines():
+    if line:
+        total += calculate_fuel_for_module(int(line))
+print(total)
